42331baa0bf402f67b9816e3d828afac29e8cd57
Aniket Nandanwar <aniketnandanwar09@gmail.com>
2023-11-26 17:44:09 +0530

    cookie cutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will help recommend top3 food item on swiggy home page.',
+    author='Aniket Nandanwar',
+    license='',
+)
